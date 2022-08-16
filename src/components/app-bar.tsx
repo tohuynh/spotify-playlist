250b3675f1e6024a3595fa169f0f539c0eaf82fc
To Huynh <huynh.nto@gmail.com>
2022-08-15 19:41:48 -0700

    Use heroicons in app bar

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -1,38 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-
-const login = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-    />
-  </svg>
-);
-
-const logout = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-    />
-  </svg>
-);
+import { LoginIcon, LogoutIcon } from "@heroicons/react/outline";
 
 export default function AppBar() {
   const { data, status } = useSession();
@@ -50,7 +17,11 @@ export default function AppBar() {
         aria-label={data ? "Sign out" : "Sign in"}
         onClick={data ? () => signOut() : () => signIn("spotify")}
       >
-        {data ? logout : login}
+        {data ? (
+          <LogoutIcon className="h-6 w-6" aria-hidden />
+        ) : (
+          <LoginIcon className="h-6 w-6" aria-hidden />
+        )}
       </button>
     </div>
   );
