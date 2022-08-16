9e759e55de155738d0214c87701003b24a1942a5
To Huynh <huynh.nto@gmail.com>
2022-08-15 19:42:04 -0700

    Add ArrayElement utility type

---
+++ src/types/utility-types.ts
@@ -0,0 +1,4 @@
+//https://stackoverflow.com/questions/41253310/typescript-retrieve-element-type-information-from-array-type
+
+export type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
