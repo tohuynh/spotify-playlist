eef242ee0e0d81663765845a1f59819b997f8ac8
To Huynh <huynh.nto@gmail.com>
2022-08-17 05:40:15 -0700

    Add create playlist dialog

--- src/components/new-playlist.tsx
+++ src/components/new-playlist.tsx
@@ -1,12 +1,139 @@
-import { useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useState,
+  Fragment,
+  FormEventHandler,
+} from "react";
 import { ArrayElement } from "../types/utility-types";
 import { inferQueryOutput, trpc } from "../utils/trpc";
 import { PlusIcon, XIcon } from "@heroicons/react/outline";
 import SearchTracks from "./search-tracks";
 import { convertDurationToHMS } from "../types/convertDurationToHMS";
 import AudioPlayer from "./audio-player";
+import { Transition, Dialog } from "@headlessui/react";
 
 type Track = ArrayElement<inferQueryOutput<"spotify.search">>;
+
+function CreatePlaylistDialog({
+  isOpen,
+  setIsOpen,
+  uris,
+}: {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  uris: string[];
+}) {
+  const createPlayList = trpc.useMutation(["spotify.createPlaylist"]);
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      playlistName: { value: string };
+      playlistDescription: { value: string };
+      playlistIsPublic: { checked: boolean };
+    };
+    if (uris.length > 0) {
+      createPlayList.mutate({
+        uris,
+        name: target.playlistName.value,
+        description: target.playlistDescription.value,
+        isPublic: target.playlistIsPublic.checked,
+      });
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => setIsOpen(false)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  New playlist
+                </Dialog.Title>
+                <form
+                  className="mt-2 flex flex-col gap-y-3"
+                  onSubmit={onSubmit}
+                >
+                  <label className="block text-xs font-medium text-gray-500">
+                    Name:
+                    <input
+                      required
+                      className="h-10 w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded outline-none"
+                      type="text"
+                      name="playlistName"
+                    />
+                  </label>
+                  <label className="block text-xs font-medium text-gray-500">
+                    Description:
+                    <input
+                      required
+                      className="h-10 w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded outline-none"
+                      type="text"
+                      name="playlistDescription"
+                    />
+                  </label>
+                  <label className="block text-md font-medium text-gray-700">
+                    <input
+                      defaultChecked={true}
+                      className="h-4 w-4 mr-2 text-sm border-2 border-gray-200 rounded outline-none accent-green-300"
+                      type="checkbox"
+                      name="playlistIsPublic"
+                    />
+                    Is public?
+                  </label>
+                  <div className="mt-4 flex justify-end items-center gap-x-4">
+                    <button
+                      type="reset"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-gray-300 px-4 py-2 text-sm font-medium focus:outline-none"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-green-300 px-4 py-2 text-sm font-medium focus:outline-none"
+                    >
+                      Create
+                    </button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
 
 function TrackChips({
   tracks,
@@ -69,7 +196,6 @@ export default function NewPlaylist() {
   };
 
   const createPlayList = trpc.useMutation(["spotify.createPlaylist"]);
-
   const onCreate = () => {
     //open the modal
     //slide from bottom, mobile
@@ -83,14 +209,21 @@ export default function NewPlaylist() {
       });
     }
   };
+  const [createPlaylistDialogIsOpen, setCreatePlaylistDialogIsOpen] =
+    useState(false);
 
   return (
     <div className="pb-20 lg:pb-0 lg:flex lg:flex-row lg:justify-start lg:gap-x-4">
+      <CreatePlaylistDialog
+        isOpen={createPlaylistDialogIsOpen}
+        setIsOpen={setCreatePlaylistDialogIsOpen}
+        uris={getRecommendationsQuery.data?.map((track) => track.uri) || []}
+      />
       <div className="lg:basis-32 flex justify-center items-start">
         <button
           className="fixed lg:relative h-16 w-16 m-4 lg:m-0 bottom-0 right-0 flex justify-center items-center bg-green-300 rounded-2xl shadow-lg"
           aria-label="New mixtape"
-          onClick={onCreate}
+          onClick={() => setCreatePlaylistDialogIsOpen(true)}
           disabled={
             getRecommendationsQuery.status === "error" ||
             getRecommendationsQuery.status === "loading"
