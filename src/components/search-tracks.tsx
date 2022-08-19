7fb8d1b73cd8445051b4b11db499944e56b964be
To Huynh <huynh.nto@gmail.com>
2022-08-18 21:40:37 -0700

    Make the combobox options have higher z index

--- src/components/search-tracks.tsx
+++ src/components/search-tracks.tsx
@@ -67,7 +67,7 @@ export default function SearchTracks(props: Props) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Combobox.Options className="absolute mt-1 max-h-60 w-full lg:w-96 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <Combobox.Options className="z-10 absolute mt-1 max-h-60 w-full lg:w-96 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
             {data?.map((track) => (
               <Combobox.Option
                 key={track.id}
