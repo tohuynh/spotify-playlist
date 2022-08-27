9a0d6fcadb0acc8f3c4fec61621a219155f88683
To Huynh <huynh.nto@gmail.com>
2022-08-27 00:06:40 -0700

    Create layout component

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,72 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
+
+import AppBar from "../../components/app-bar";
+
+type LayoutProps = {
+  title: string
+  description: string
+  children: ReactNode
+}
+const Layout = ({title, description, children}: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <main className="min-h-screen bg-slate-100 text-slate-900">
+        <AppBar />
+        {children}
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            duration: 3000,
+            success: {
+              iconTheme: {
+                primary: "#1db954",
+                secondary: "#ffffff",
+              },
+            },
+            error: {
+              iconTheme: {
+                primary: "#EF4444",
+                secondary: "#ffffff",
+              },
+            },
+            ariaProps: {
+              role: "status",
+              "aria-live": "polite",
+            },
+          }}
+        />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
-import Head from "next/head";
-import { Toaster } from "react-hot-toast";
 
-import AppBar from "../components/app-bar";
+import Layout from "../components/layout";
 import NewPlaylist from "../components/new-playlist";
 import Welcome from "../components/welcome";
 
@@ -15,59 +13,9 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <title>Tiny Mixtapes</title>
-        <meta name="description" content="Create Spotify mixtapes" />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
-      <main className="min-h-screen bg-slate-100 text-slate-900">
-        <AppBar />
+      <Layout title="Tiny Mixtapes" description="Create Spotify mixtapes">
         {data ? <NewPlaylist /> : <Welcome />}
-        <Toaster
-          position="bottom-center"
-          toastOptions={{
-            duration: 3000,
-            success: {
-              iconTheme: {
-                primary: "#1db954",
-                secondary: "#ffffff",
-              },
-            },
-            error: {
-              iconTheme: {
-                primary: "#EF4444",
-                secondary: "#ffffff",
-              },
-            },
-            ariaProps: {
-              role: "status",
-              "aria-live": "polite",
-            },
-          }}
-        />
-      </main>
+      </Layout>
     </>
   );
 };
