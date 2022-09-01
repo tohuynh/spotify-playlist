bf4f1e5e8177956cd50b9360ae31a3437055d60f
To Huynh <huynh.nto@gmail.com>
2022-08-31 21:52:54 -0700

    Change to ul and li

--- src/pages/playlists.tsx
+++ src/pages/playlists.tsx
@@ -2,7 +2,7 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 import Layout from "../components/layout";
 import Spinner from "../components/new-playlist/spinner";
@@ -13,8 +13,7 @@ type Playlists = inferQueryOutput<"spotify.getPlaylists">["items"];
 
 const Playlists: NextPage = () => {
   const { data: sessionData, status } = useSession();
-  const [playlists, setPlaylists] = useState<Playlists>([]);
-  const { fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
+  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
     trpc.useInfiniteQuery(
       [
         "spotify.getPlaylists",
@@ -25,21 +24,9 @@ const Playlists: NextPage = () => {
       {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
         refetchOnWindowFocus: false,
-        onSuccess: (data) => {
-          console.log(playlists);
-          console.log(data);
-          const lastPage = data.pages.at(-1);
-          if (lastPage && playlists.length < lastPage.total) {
-            if (data.pages.length == 1) {
-              setPlaylists([...lastPage.items]);
-            } else {
-              setPlaylists((prev) => [...prev, ...lastPage.items]);
-            }
-          }
-        },
       }
     );
-  const [listRef] = useAutoAnimate<HTMLDivElement>();
+  const [listRef] = useAutoAnimate<HTMLUListElement>();
 
   useEffect(() => {
     function onScroll() {
@@ -70,75 +57,82 @@ const Playlists: NextPage = () => {
           <div className="flex justify-center py-4">
             <Spinner isLoading={isFetching && !isFetchingNextPage} />
           </div>
-          <div
+          <ul
             ref={listRef}
-            className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4"
+            className="grid grid-cols-1 gap-20 md:grid-cols-2 xl:grid-cols-4"
           >
-            {playlists.map((playlist) => {
+            {data?.pages.map((page, i) => {
               return (
-                <div className="bg-white" key={playlist.id}>
-                  <div className="relative aspect-video w-full">
-                    <Image
-                      alt={`Covert of ${playlist.name}`}
-                      src={playlist.image.url}
-                      layout="fill"
-                    />
-                  </div>
-                  <div className="p-4 pt-2">
-                    <a
-                      className="block truncate text-lg font-semibold underline underline-offset-4 md:text-2xl"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href={playlist.uri}
-                    >
-                      {playlist.name}
-                    </a>
-                    <div className="truncate text-sm text-zinc-500 md:text-base">
-                      {playlist.description}
-                    </div>
-                    <div className="mt-4 grid grid-cols-1 gap-1">
-                      <label className="block">
-                        Intensity:
-                        <input
-                          readOnly
-                          className="w-full accent-spotify-green outline-none"
-                          type="range"
-                          min={0}
-                          max={100}
-                          value={playlist.audioFeatures.energy}
-                        />
-                      </label>
-                      <label className="block">
-                        Danceability:
-                        <input
-                          readOnly
-                          className="w-full accent-spotify-green outline-none"
-                          type="range"
-                          min={0}
-                          max={100}
-                          value={playlist.audioFeatures.danceability}
-                        />
-                      </label>
-                      <label className="block">
-                        Positivity:
-                        <input
-                          readOnly
-                          className="w-full accent-spotify-green outline-none"
-                          type="range"
-                          min={0}
-                          max={100}
-                          value={playlist.audioFeatures.valence}
-                        />
-                      </label>
-                      <div>
-                        {`Beats per minute: ${playlist.audioFeatures.tempo}`}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <Fragment key={i}>
+                  {page.items.map((playlist) => {
+                    return (
+                      <li className="rounded-md bg-white" key={playlist.id}>
+                        <div className="relative aspect-video w-full">
+                          <Image
+                            className="rounded-t-md"
+                            alt={`Covert of ${playlist.name}`}
+                            src={playlist.image.url}
+                            layout="fill"
+                          />
+                        </div>
+                        <div className="p-4 pt-2">
+                          <a
+                            className="block truncate text-lg font-semibold underline underline-offset-4 md:text-2xl"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href={playlist.uri}
+                          >
+                            {playlist.name}
+                          </a>
+                          <div className="truncate text-sm text-zinc-500 md:text-base">
+                            {playlist.description}
+                          </div>
+                          <div className="mt-4 grid grid-cols-1 gap-1">
+                            <label className="block">
+                              Intensity:
+                              <input
+                                readOnly
+                                className="w-full accent-spotify-green outline-none"
+                                type="range"
+                                min={0}
+                                max={100}
+                                value={playlist.audioFeatures.energy}
+                              />
+                            </label>
+                            <label className="block">
+                              Danceability:
+                              <input
+                                readOnly
+                                className="w-full accent-spotify-green outline-none"
+                                type="range"
+                                min={0}
+                                max={100}
+                                value={playlist.audioFeatures.danceability}
+                              />
+                            </label>
+                            <label className="block">
+                              Positivity:
+                              <input
+                                readOnly
+                                className="w-full accent-spotify-green outline-none"
+                                type="range"
+                                min={0}
+                                max={100}
+                                value={playlist.audioFeatures.valence}
+                              />
+                            </label>
+                            <div>
+                              {`Beats per minute: ${playlist.audioFeatures.tempo}`}
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </Fragment>
               );
             })}
-          </div>
+          </ul>
         </>
       ) : (
         <Welcome />
